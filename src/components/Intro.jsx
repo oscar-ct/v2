832cbe528b920e176b6d6ed92fe50b0ae6cd6b15
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-05 23:21:12 -0500

    feat: created new component, import nav and about to complete layout

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Headshot from "../bw-headshot.jpeg";
+import Navbar from "./Navbar";
+import About from "./About";
+
+
+const Intro = () => {
+    return (
+        <>
+            <div className={"relative"}>
+                <img className="w-screen bg-center bg-no-repeat bg-cover min-h-[77rem]" src={"https://wallpapercrafter.com/desktop/13142-hill-night-horizon-dark-minimalism-4k.jpg"}/>
+                <div className="absolute opacity-100 inset-0 z-0 flex flex-col items-center justify-between w-100">
+                    <Navbar/>
+                    <About/>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Intro;
