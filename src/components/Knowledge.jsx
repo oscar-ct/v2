a986c7e85923183ef1e92996b86d281a06d838c3
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-08 20:18:20 -0500

    feat: resized icons, other minor updates

--- src/components/Knowledge.jsx
+++ src/components/Knowledge.jsx
@@ -29,43 +29,43 @@ const Knowledge = () => {
                         <div className={"text-4xl pb-[4rem] font-bold text-purple-500"}>
                             What I Know
                         </div>
-                        <div className={"flex px-3 flex-col lg:flex-row text-white"}>
+                        <div className={"flex lg:px-3 flex-col lg:flex-row text-white"}>
                             <div className={"flex flex-col w-100 lg:w-6/12"}>
                                 <div className={"text-3xl font-bold mb-5"}>
                                     Front-End Development
                                 </div>
-                                <div className={"p-2"}>
+                                <div className={"lg:p-3"}>
                                     <p className={"font-light"}>
-                                        Using <span className={"font-bold text-purple-400"}>JavaScript</span> libraries such as <span className={"font-bold text-purple-400"}>React</span> and <span className={"font-bold text-purple-400"}>jQuery</span> has made a frontend development a breeze.  jQuery's DOM traversing is unmatched and React's vast hook library is extremely useful and powerful, paired with <span className={"font-bold text-purple-400"}>Redux</span> Toolkit for state handling allows me to provide users with a fast, seamless experience.
+                                        Using <span className={"font-bold text-purple-400"}>JavaScript</span> libraries such as <span className={"font-bold text-purple-400"}>React</span> and <span className={"font-bold text-purple-400"}>jQuery</span> has made a frontend development a breeze.  jQuery's DOM traversing/manipulating is unmatched and React's vast hook library is extremely useful and powerful, paired with <span className={"font-bold text-purple-400"}>Redux</span> Toolkit for state handling allows me to provide users with fast and seamless experiences.
                                     </p>
                                     <p className={"font-light"}>
-                                        <span className={"font-bold text-purple-400"}>TailwindCSS</span> and <span className={"font-bold text-purple-400"}>Bootstrap</span> are more tools that I have taken full advantage of when developing a new application.  Learning both is tricky but the speed, simplicity, and customization is unparalleled.
+                                        <span className={"font-bold text-purple-400"}>TailwindCSS</span> and <span className={"font-bold text-purple-400"}>Bootstrap</span> are more tools that I have taken full advantage of when developing an application.  Learning both is tricky but the speed, simplicity, and customization is unparalleled.
                                     </p>
                                 </div>
                                 <div className={"flex flex-wrap p-6"}>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={JavaScript} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={JavaScript} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={HTML} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={HTML} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={CSS} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={CSS} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={ReactIcon} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={ReactIcon} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={Redux} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Redux} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={JQuery} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={JQuery} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={Bootstrap} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Bootstrap} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={Tailwind} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Tailwind} alt={"JS"}/>
                                     </icon>
                                 </div>
                                 {/*<div className={"text-3xl font-bold mb-5"}>*/}
@@ -73,11 +73,11 @@ const Knowledge = () => {
                                 {/*</div>*/}
                             </div>
 
-                            <div className={"flex px-3 flex-col w-100 lg:w-6/12"}>
+                            <div className={"flex lg:px-3 flex-col w-100 lg:w-6/12"}>
                                 <div className={"text-3xl font-bold mb-5"}>
                                     Back-End Development
                                 </div>
-                                <div className={"p-2"}>
+                                <div className={"lg:p-3"}>
                                     <p className={"font-light pb-2 border-b-2 border-dotted"}>
                                         Programming applications in <span className={"font-bold text-purple-400"}>Java EE</span>, with Servlets and JavaServer Pages, as well as utilizing Prepared Statements to prevent SQL Injection has provided me a wide-ranging experience with legacy programming.
                                     </p>
@@ -85,39 +85,39 @@ const Knowledge = () => {
                                         Setting up a full-stack application using <span className={"font-bold text-purple-400"}>Spring Boot</span> is amazingly simple. Creating a <span className={"font-bold text-purple-400"}>MySQL</span> database with Java Beans takes the complicated schema business out of the picture. Defining controllers with Spring couldn't be any easier and by utilizing <span className={"font-bold text-purple-400"}>Thymeleaf</span>, serving the frontend is effortless.
                                     </p>
                                     <p className={"pt-2 font-light pb-2 border-b-2 border-dotted"}>
-                                        <span className={"font-bold text-purple-400"}>Express</span> is my go to framework for created a <span className={"font-bold text-purple-400"}>Node.js</span> backend. Express provides a robust set of features and paired with <span className={"font-bold text-purple-400"}>Mongoose</span> for Object modeling, creating <span className={"font-bold text-purple-400"}>MongoDB</span> collections is quick and easy.
+                                        <span className={"font-bold text-purple-400"}>Express</span> is my go to framework for creating a <span className={"font-bold text-purple-400"}>Node.js</span> backend. Express provides a robust set of features and paired with <span className={"font-bold text-purple-400"}>Mongoose</span> for Object modeling, creating <span className={"font-bold text-purple-400"}>MongoDB</span> collections is quick and painless.
                                     </p>
                                     <p className={" pt-2 font-light"}>
-                                        <span className={"font-bold text-purple-400"}>Firebase</span> is another great tool under my belt. Everything from storage to user authentication, Google's Firebase brings it all together without any confusion.
+                                        <span className={"font-bold text-purple-400"}>Firebase</span> is another great tool under my belt. Everything from storage to user authentication, Google's Firebase brings it all together without any of the extra legwork.
                                     </p>
                                 </div>
                                 <div className={"flex flex-wrap p-6"}>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={Java} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Java} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={Spring} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Spring} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={Thymeleaf} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Thymeleaf} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={MySQL} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={MySQL} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={"w-12"} src={Express} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Express} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={Node} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Node} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={MongoDB} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={MongoDB} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={Mongoose} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Mongoose} alt={"JS"}/>
                                     </icon>
                                     <icon className={"m-3"}>
-                                        <img className={" w-12"} src={Firebase} alt={"JS"}/>
+                                        <img className={"w-10 lg:w-12"} src={Firebase} alt={"JS"}/>
                                     </icon>
                                 </div>
                             </div>
