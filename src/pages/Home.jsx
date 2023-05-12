fbd6bfb74ae0771d6168e8db5c7e1131db97927b
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-12 01:16:27 -0500

    removed nav

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Knowledge from "../components/Knowledge";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
-import Navbar from "../components/Navbar";
 import About from "../components/About";
 
 const Home = () => {
     return (
         <>
-            <Navbar/>
             <About/>
             <Knowledge/>
             <Projects/>
