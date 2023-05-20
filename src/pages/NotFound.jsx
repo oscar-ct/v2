d04273c653ceef4d7b4f19a081895bb8ce19e6ea
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-20 00:19:11 -0500

    feat: updated btn

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div className="hero h-screen">
+        <div className="hero">
             <div className="text-center hero-content">
                 <div className="max-w-lg text-white">
                     <h1 className="text-8xl font-bold mb-8"> Oops!
@@ -11,9 +11,13 @@ const NotFound = () => {
                     <p className="text-5xl mb-8">
                         404 - Page not found!
                     </p>
-                    <Link  to="/" className="btn btn-primary btn-lg">
-                        <span className="mr-2"/>
-                        Back To Home
+                    <Link to="/" className={"flex justify-center"}>
+                        <button
+                            style={{cursor: "pointer"}}
+                            className="antialiased text-center hover:subpixel-antialiased text-lg mb-2 block rounded-lg bg-gradient-to-tr from-purple-600 to-purple-400 py-2 px-4 font-bold uppercase text-white shadow-md shadow-purple-500/20 transition-all hover:shadow-lg hover:shadow-purple-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                        >
+                            <span>BACK TO PORTFOLIO</span>
+                        </button>
                     </Link>
                 </div>
             </div>
