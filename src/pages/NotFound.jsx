354fa6ffbdcbeff3f0be86bda2976b76c17e52e5
Oscar Castro <oscar.a.castro818@gmail.com>
2023-05-20 00:00:06 -0500

    feat: setup not found page, styled

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            Work In Progress
+        <div className="hero h-screen">
+            <div className="text-center hero-content">
+                <div className="max-w-lg text-white">
+                    <h1 className="text-8xl font-bold mb-8"> Oops!
+                    </h1>
+                    <p className="text-5xl mb-8">
+                        404 - Page not found!
+                    </p>
+                    <Link  to="/" className="btn btn-primary btn-lg">
+                        <span className="mr-2"/>
+                        Back To Home
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
